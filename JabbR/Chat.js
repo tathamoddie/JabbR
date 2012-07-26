d7801b61e3f82a9d0d13c369e52c0115d9795f24
Sam Moore <samandmoore@gmail.com>
2012-07-26 14:54:19 -0400

    added utility function for logging and changed initialization to use utility method to check jabbr.logging cookie

--- JabbR/Chat.js
+++ JabbR/Chat.js
@@ -918,7 +918,7 @@
         ui.addMessage('Type /help to see the list of commands', 'notification');
 
         function initConnection() {
-            var logging = $.cookie('jabbr.logging') === '1',
+            var logging = utility.isLoggingEnabled(),
                 transport = $.cookie('jabbr.transport'),
                 options = {};
 
